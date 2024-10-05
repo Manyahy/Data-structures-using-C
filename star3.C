7ed17c0120330d71e3afc5accb24aaf373ccf277
Manya H Y <hymanya5@gmail.com>
2024-10-05 16:32:08 +0530

    Update

---
+++ star3.C
@@ -0,0 +1,45 @@
+
+
+#include<conio.h>
+
+#include<stdio.h>
+
+void main()
+
+{
+
+int i,j,k;
+
+for(i=5;i>=1;i--)
+
+STA
+
+tab
+
+clrscr();
+
+{
+
+for(j=1;j<=i-1;j++)
+
+{
+
+printf(" ");
+
+}
+
+for(k=5;k>=i;k--)
+
+{
+
+printf("*");
+
+}
+
+printf("\n");
+
+}
+
+getch();
+
+}

795dbf087c0e1cfce22fcff3c717d5ee8db253fb
Manya H Y <hymanya5@gmail.com>
2025-01-24 15:38:43 +0530

    Update sparseTriplettransformation.c

--- sparseTriplettransformation.c
+++ sparseTriplettransformation.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
-int a[m][n],i,j,m,n,b[5][3];
-printf('Enter the size of row and column:");
+int a[5][5],i,j,m,n,b[5][3],count=0;
+printf("Enter the size of row and column:");
 scanf("%d%d",&m,&n);
-printf("Enetr a sparse matrix");
+printf("Enetr a sparse matrix:");
 for(int i=0;i<m;i++){
 for(int j=0;j<n;j++){
 scanf("%d",&a[i][j]);}}
-for(int i=0;i<3;i++){
-for(int j=0;j<3;j++){
-if(a[j][j]!=0){
+for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+if(a[i][j]!=0){
 count++;
 b[count][0]=i;
 b[count][1]=j;
@@ -18,10 +18,13 @@ b[count][2]=a[i][j];
 b[0][0]=m;
 b[0][1]=n;
 b[0][2]=count;
-printf("Triplet transformation:");
-for(int i=0;i<count;i++){
+printf("Triplet transformation:\n");
+for(int i=0;i<=count;i++){
 for(int j=0;j<3;j++){
-printf("%d",a[i][j]);
-}}}
-
+printf("%d\t",b[i][j]);
+}
+    printf("\n");
+}
+    
+}
 
